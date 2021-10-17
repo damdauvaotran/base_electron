e4fec6ddbab7e3f7a487144e91459471a5ebc3f9
damdauvaotran <ngokhoaisan1999@gmail.com>
2021-10-17 14:57:35 +0700

    Change to default electron

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(
+    <>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </>,
+    document.body
+  );
 }
 
 render();
